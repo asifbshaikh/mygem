9709cb4acfd36244759207f9b87137e532cc055e
ASIF SHAIKH <asif.shaikh@techbulls.com>
2018-10-31 15:00:09 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Reference
+
+https://www.sitepoint.com/creating-your-first-gem/
---
+++ lib/awesome_gem.rb
@@ -0,0 +1,7 @@
+module AwesomeGem
+  class WhoIs
+    def self.awesome?
+      puts "welcome to first Gem of Asif"
+    end
+  end
+end
